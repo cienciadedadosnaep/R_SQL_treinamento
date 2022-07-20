dddb0355e13f3d1808db25a25278caa9a24b7699
Robson Wilson Silva Pessoa <robsonpessoa2007@gmail.com>
2022-07-20 18:06:07 -0300

    todos dados educação SSA

--- leitura_verificaca_dbreconhecendosalvador.R
+++ leitura_verificaca_dbreconhecendosalvador.R
@@ -14,23 +14,43 @@ db_host <- '127.0.0.1' # for local access
 db_port <- 3309
 
 # 3. Read data from db
+
+## docentes
 mydb <-  dbConnect(MySQL(), user = db_user, password = db_password,
                    dbname = db_name, host = db_host, port = db_port)
-
-s <- paste0("select id_aluno,nu_ano,nu_idade,tp_sexo from ", "alunos_bas_med")
-
+s <- paste0("select * from ", "docentes_bas_med")
 rs <- dbSendQuery(mydb, s)
-
 df<- NULL
 df <-  fetch(rs, n = -1)
 
-library(dplyr)
-library(ggplot2)
-df %>% filter(nu_ano>2000) %>% 
-          group_by(nu_idade) %>% 
-          summarise(n = n()) %>% ggplot(aes(x=nu_idade,y=n)) +
-                                 geom_col()
 
+## escolas
+#mydb <-  dbConnect(MySQL(), user = db_user, password = db_password,
+#                   dbname = db_name, host = db_host, port = db_port)
+escola <- paste0("select * from ", "escolas_bas_med")
+rescolas <- dbSendQuery(mydb, escola)
+
+dfesc<- NULL
+dfesc <-  fetch(rescolas, n = -1)
+
+## nomes escolas
+#mydb <-  dbConnect(MySQL(), user = db_user, password = db_password,
+#                   dbname = db_name, host = db_host, port = db_port)
+nescola <- paste0("select * from ", "nome_escola_bas_med")
+nrescolas <- dbSendQuery(mydb, nescola)
+
+dfnesc<- NULL
+dfnesc <-  fetch(nrescolas, n = -1)
+
+
+## alunos escolas
+#mydb <-  dbConnect(MySQL(), user = db_user, password = db_password,
+#                   dbname = db_name, host = db_host, port = db_port)
+alunos <- paste0("select * from ", "alunos_bas_med")
+nalunos <- dbSendQuery(mydb, alunos)
+
+dfaluno<- NULL
+dfaluno <-  fetch(nalunos, n = -1)
 
 
 
