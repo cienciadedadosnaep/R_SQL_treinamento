319bdaa9e1ac7bca05e15e81a2c0b0e2a2ea3fa3
Robson Wilson Silva Pessoa <robsonpessoa2007@gmail.com>
2022-11-13 21:50:48 -0300

    codigo inep 12 colégios participantes

--- codigoreorientacao.R
+++ codigoreorientacao.R
@@ -1,4 +1,6 @@
-#01 - Leitura banco de dados
+#00 - Leitura 
+
+#01 - Leitura Banco de Dados
 
 library(readr)
 
@@ -8,3 +10,19 @@ datas <- read_csv("data/datas.csv", col_types = cols(data = col_datetime(format 
 dadoscd2022 <- read_csv("data/dadoscd2022.csv")
 # dados da avaliacao anual de PI
 dadospi2022 <- read_csv("data/dadospi2022.csv")
+
+
+
+## Qual e a situacao do IDEB das escolas parceiras e da cidade
+
+colegiosp_siglas <- c("CEHMC","CEY","CSB","CEMCN",
+                      "CEEV","CEMATF","CEES","CEMD",
+                      "CEMN","CENTRAL","CMCJ","IFBA")
+codigos_inep_parceiras <- c(29192790,29182182,29181550,29193699,
+                            29182158,29185394,29180490,29181500,
+                            29182131,29182123,29190061,29196442)
+
+
+## Mapa das escolas com respectivos IDEBs 
+
+##
