d55196042c569628da1bc8ccb7b6c2053cbaf109
Robson Wilson Silva Pessoa <robsonpessoa2007@gmail.com>
2023-02-23 22:20:44 -0300

    troca de banco de dados. Install tidysynth

--- quasi_experimental.R
+++ quasi_experimental.R
@@ -4,7 +4,7 @@ library(readr)
 library(dplyr)
 
 resultadoCD2021 <- read_csv("data/resultado_CD.csv")
-resultadoCD2022 <- dadoscd2022
+resultadoCD2022 <- dadoscd2022 <- read_csv("data/dadoscd2022_V00.csv")
 
 
 notas2021 <- resultadoCD2021 %>% select(ID,QUANTITATIVO_R)
@@ -14,3 +14,12 @@ notas2022 <- resultadoCD2022 %>% select(ID,quantitativog1,quantitativog2)
 
 
 tabela <- full_join(notas2021,notas2022,"ID")
+
+
+# metodo
+
+library(tidysynth)
+
+
+
+
