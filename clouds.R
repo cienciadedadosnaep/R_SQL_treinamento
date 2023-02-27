9766ab06268fccdbee85a7eb4f594220ad5f5e1f
Robson Wilson Silva Pessoa <robsonpessoa2007@gmail.com>
2023-02-27 13:48:40 -0300

    Tree map

--- clouds.R
+++ clouds.R
@@ -21,3 +21,34 @@ auxCorpus <- tm_map(auxCorpus, removeWords, stopwords('pt'))
 #auxCorpus <- tm_map(auxCorpus, stemDocument)
 
 wordcloud(auxCorpus$content,max.words=500,colors=c("blue","red"))
+
+
+d <- palavrasseeufossecientista_v00
+
+
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+
+tabelassa <- get_sidra(136,geo = 'City',geo.filter = '2927408')
+
+
+# install.packages("treemapify")
+library(treemapify)
+# install.packages("ggplot2")
+library(ggplot2)
+library(dplyr)
+
+
+
+tabelassa %>% 
+  filter(`Cor ou raça` != c('Total')) %>%
+  summarise( `Cor ou raça`,Valor = round(Valor*100/sum(Valor),1)) %>%
+  ggplot( aes(area = Valor, fill =`Cor ou raça` , label = Valor),
+          color=brewer.pal(6, "Dark2")) +
+  geom_treemap() +
+  geom_treemap_text()
+
+
